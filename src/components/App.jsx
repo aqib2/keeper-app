a68b8b91ee18252d444218830f38c19dc28e82f3
mohammed aqib <grifin096@gmail.com>
2021-05-02 19:22:17 +0530

    render all the notes from notes.js

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNote(note) {
+   return (
+       <Note 
+           name = {note.name}
+           content = {note.content}
+       />
+   )
+}
+
+function App() {
+    return (
+        <div>
+            <Header />
+            {notes.map(createNote)}
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
